d2bdddccf000a2d1e052ccbba7b96949dc25d8b5
pointschan <pointschan@users.noreply.github.com>
2015-03-03 11:23:05 -0500

    create test file for odbchelper.py

---
+++ tests/test_odbchelper.py
@@ -0,0 +1,41 @@
+__author__ = 'pointschan'
+
+from collections import OrderedDict
+from odbchelper import *
+import unittest
+
+
+class testBuildConnectionStringFunctions(unittest.TestCase):
+
+    def setUp(self):
+        self.params = OrderedDict([("server","mpilgrim"),
+                ("database","master"),
+                ("uid","sa"),
+                ("pwd","secret")
+                ])
+        self.paramsx = {"server":"mpilgrim",
+                    "database":"master",
+                    "uid":"sa",
+                    "pwd":"secret"
+                    }
+
+    def testBuildConnectionString(self):
+        """Build a connection string from a dictionaryof parameters.
+
+        Returns string."""
+        print self.params
+        dbConnectionString = buildConnectionString(self.params)
+        print dbConnectionString
+        self.assertEqual(dbConnectionString, "server=mpilgrim;database=master;uid=sa;pwd=secret")
+
+
+    def testBuildConnectionStringx(self):
+        print self.paramsx
+        dbConnectionStringx = buildConnectionString(self.paramsx)
+        print dbConnectionStringx
+        self.assertNotEqual(dbConnectionStringx, "server=mpilgrim;database=master;uid=sa;pwd=secret")
+
+print __name__
+if __name__ == "__main__":
+    unittest.main()
+
